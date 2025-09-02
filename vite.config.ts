a36f4b75ef4f6482da4bf28fbf1f949da017d849
Isaac <isaac.acevedor1610@gmail.com>
2025-09-02 00:30:19 -0300

    Correccion de vista 7

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,13 @@
-// vite.config.ts
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { fileURLToPath, URL } from 'node:url'
 
 export default defineConfig({
-  base: '/Orange-Blue/',
+  base: '/Orange-Blue/', // nombre EXACTO del repo
   plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
 })
