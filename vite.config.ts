7a75bb8c855c858ca58f37e89a5d1fe063793f51
Isaac <isaac.acevedor1610@gmail.com>
2025-09-02 00:16:04 -0300

    Correccion de vista 4

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-  base: '/BLUE-ORANGE/',
+  base: '/Orange-Blue/',
   plugins: [vue()],
 })
