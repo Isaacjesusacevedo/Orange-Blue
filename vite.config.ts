ec8e96dff1fb45ad3222c1b5021b195425d6dca7
Isaac <isaac.acevedor1610@gmail.com>
2025-09-02 00:08:41 -0300

    Correccion de vista 2

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,10 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
 
 export default defineConfig({
-  base: '/lag-comunity-frontend/',
-  plugins: [vue(), vueDevTools()],
+  base: '/blue-orange/',
+  plugins: [vue()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
